9ed170921c2e53410bc9cc311cef188f1c699e7e
devopsasp <peterone23four@gmail.com>
2024-08-12 07:59:13 +0530

    all files'

---
+++ setup.py
@@ -0,0 +1,52 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='flaskapp',
+    version='1.0',
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+        'blinker',
+'certifi',
+'charset-normalizer',
+'click',
+'colorama',
+'contourpy',
+'cycler',
+'Flask',
+'fonttools',
+'idna',
+'itsdangerous',
+'Jinja2',
+'joblib',
+'kiwisolver',
+'MarkupSafe',
+'matplotlib',
+'numpy',
+'packaging'
+'pandas',
+'pillow',
+'PyAudio',
+'pyodbc',
+'pyparsing',
+'python-dateutil',
+'pytz',
+'requests'
+'scikit-learn',
+'scipy',
+'setuptools',
+'simpy',
+'six',
+'SpeechRecognition',
+'threadpoolctl',
+'typing_extensions',
+'tzdata',
+'urllib3',
+'Werkzeug'
+
+        
+    ],
+    author='asp',
+    author_email='arjnu_sailesh@yahoo.com',
+    description='A Flask app that does something cool'
+)
